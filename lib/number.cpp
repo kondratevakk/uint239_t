75221d7e5dc0ca565791b55f2f32a0f25a47307a
Kondrateva Ekaterina <kondrateva-ekaterina@ro.ru>
2025-12-24 03:19:55 +0300

    Add files via upload

---
+++ lib/number.cpp
@@ -0,0 +1,44 @@
+#include "number.h"
+
+
+uint239_t FromInt(uint32_t value, uint32_t shift) {
+    return uint239_t();
+}
+
+uint239_t FromString(const char* str, uint32_t shift) {
+    return uint239_t();
+}
+
+uint239_t operator+(const uint239_t& lhs, const uint239_t& rhs) {
+    return uint239_t();
+}
+
+uint239_t operator-(const uint239_t& lhs, const uint239_t& rhs) {
+    return uint239_t();
+}
+
+uint239_t operator*(const uint239_t& lhs, const uint239_t& rhs) {
+    return uint239_t();
+}
+
+uint239_t operator/(const uint239_t& lhs, const uint239_t& rhs) {
+    return uint239_t();
+}
+
+bool operator==(const uint239_t& lhs, const uint239_t& rhs) {
+    return false;
+}
+
+bool operator!=(const uint239_t& lhs, const uint239_t& rhs) {
+    return false;
+}
+
+std::ostream& operator<<(std::ostream& stream, const uint239_t& value) {
+    stream << "Implement me";
+
+    return stream;
+}
+
+uint64_t GetShift(const uint239_t& value) {
+    return 0;
+}
